28f058762aeee454f0243f9b5abe5ff9f4d592eb
teotsi <teotsi@gmail.com>
2020-03-12 17:25:55 +0200

    Long live Nuxt.js!

---
+++ components/LoginComponent.vue
@@ -0,0 +1,116 @@
+<template>
+  <div id="login">
+    <header>
+      <a href="#"><h1>Unfold</h1></a>
+    </header>
+    <div class="container"
+         v-bind:class="{'right-panel-active':rightPanelActive}"
+         id="container">
+      <Form cls="sign-up-container" msg="Sign up"/>
+      <Form cls="sign-in-container" msg="Sign in"/>
+      <OverlayComponent v-on:toggle-class="rightPanelActive=!rightPanelActive"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Form from "./Form";
+  import OverlayComponent from "./OverlayComponent";
+
+  export default {
+    name: 'LoginComponent',
+    components: {
+      OverlayComponent,
+      Form
+    },
+    data: function () {
+      return {
+        rightPanelActive: false
+      };
+    }
+
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,800');
+
+  #login {
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  @media only screen and (max-width: 600px) {
+    .container {
+      width: 50%;
+      height: 50%;
+    }
+
+    .container {
+      position: absolute;
+      top: 20%;
+      left: 0%;
+      right: 0%;
+    }
+  }
+
+  header {
+    background: linear-gradient(35deg, #CCFFFF, #FFCCCC);
+    padding: 20px 10px;
+  }
+
+  .container {
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25),
+    0 10px 10px rgba(0, 0, 0, 0.22);
+    position: absolute;
+    top: 20%;
+    right: 7.5%;
+    overflow: hidden;
+    width: 768px;
+    max-width: 100%;
+    min-height: 480px;
+  }
+
+
+  body {
+    background: #f6f5f7;
+    /* */
+    /* height: 100vh;*/
+    margin: 0px;
+  }
+
+  h1 {
+    font-weight: bold;
+    margin: 0;
+    color: black;
+  }
+
+  h2 {
+    text-align: center;
+    color: black;
+  }
+
+  p {
+    font-size: 14px;
+    font-weight: 100;
+    line-height: 20px;
+    letter-spacing: 0.5px;
+    margin: 20px 0 30px;
+    color: black;
+    padding: 25px;
+  }
+
+  span {
+    font-size: 12px;
+  }
+
+  a {
+    color: #333;
+    font-size: 14px;
+    text-decoration: none;
+    margin: 15px 0;
+  }
+
+
+</style>
---
+++ components/OverlayComponent.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="overlay-container">
+        <div class="overlay">
+            <div class="overlay-panel overlay-left">
+                <h1>Hello There!</h1>
+                <p>Enter your personal details and start your journey with us</p>
+                <ButtonComponent v-on:click.native="$emit('toggle-class')" is-ghost msg="Sign up" id="signUp" />
+            </div>
+            <div class="overlay-panel overlay-right">
+                <h1>Good to have you back!</h1>
+                <p>To keep connected with us please login with your personal info</p>
+                <ButtonComponent v-on:click.native="$emit('toggle-class')" is-ghost msg="Sign in" id="signIn"/>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import ButtonComponent from "./ButtonComponent";
+
+    export default {
+        name: "OverlayComponent",
+        components: {ButtonComponent},
+        methods: {
+        }
+    }
+</script>
+
+<style scoped>
+    .overlay {
+        background: linear-gradient(35deg, #CCFFFF, #FFCCCC);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: 0 0;
+        color: #FFFFFF;
+        position: relative;
+        left: -100%;
+        height: 100%;
+        width: 200%;
+        transform: translateX(0);
+        transition: transform 0.6s ease-in-out;
+    }
+
+    .overlay-container {
+        position: absolute;
+        top: 0;
+        left: 50%;
+        width: 50%;
+        height: 100%;
+        overflow: hidden;
+        transition: transform 0.6s ease-in-out;
+        z-index: 100;
+    }
+
+    .container.right-panel-active .overlay-container {
+        transform: translateX(-100%);
+    }
+
+    .overlay-panel {
+        position: absolute;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        padding: 60px;
+        text-align: center;
+        top: 0;
+        height: 100%;
+        width: 50%;
+        transform: translateX(0);
+        transition: transform 0.6s ease-in-out;
+    }
+
+    .overlay-left {
+        transform: translateX(-20%);
+
+    }
+
+    .overlay-right {
+        right: 0;
+        transform: translateX(0);
+    }
+
+    .container.right-panel-active .overlay {
+        transform: translateX(50%);
+    }
+
+
+    .container.right-panel-active .overlay-left {
+        transform: translateX(0);
+    }
+
+    .container.right-panel-active .overlay-right {
+        transform: translateX(20%);
+    }
+
+
+</style>
