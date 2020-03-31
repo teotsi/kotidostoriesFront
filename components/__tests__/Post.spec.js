78d3be79a0642762a49dc61e6567db9ab3a23e4d
teotsi <teotsi@gmail.com>
2020-04-01 01:21:32 +0300

    added props to test

--- components/__tests__/Post.spec.js
+++ components/__tests__/Post.spec.js
@@ -4,6 +4,14 @@ import Post from "../LandingPage/Post";
 
 const factory = () => {
   return shallowMount(Post, {
+    propsData:{
+      title:"Testy story",
+      preview:"Testy preview that actually turned out waaaay too long so it will sadly be truncated",
+      id:"42",
+      content:"A lonely gray couch!",
+      date:"05/05/05",
+      user:"testidis"
+    }
   });
 };
 
