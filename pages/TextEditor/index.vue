65217dce33c3ecca38b6b53180d273ed7ff165e3
teotsi <teotsi@gmail.com>
2020-04-04 05:29:44 +0300

    added more fonts and stuff, cleaned up dependencies

--- pages/TextEditor/index.vue
+++ pages/TextEditor/index.vue
@@ -1,88 +1,49 @@
 <template>
-  <section class="container">
-    <client-only>
-      <quill-editor
-        :options="editorOption"
-        @blur="onEditorBlur($event)"
-        @focus="onEditorFocus($event)"
-        @ready="onEditorReady($event)"
-        ref="editor"
-        v-model="content"
-      />
-      <div class="save__buttons">
-        <b-button variant="outline-danger">Cancel</b-button>
-        <b-button>Publish</b-button>
-        <b-button id="buttonSave">Save</b-button>
-      </div>
-    </client-only>
-  </section>
+  <div class="container">
+    <editor></editor>
+    <div class="save__buttons">
+      <b-button variant="outline-danger">Cancel</b-button>
+      <b-button>Publish</b-button>
+      <b-button id="buttonSave">Save</b-button>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'quill-example-nuxt',
-    middleware: "redirectLogin",
-    data() {
-      return {
-        content: `
-          <h3>
-            Hi there,
-          </h3>
-          <p>
-            You can write here your story!
-          </p>
-          <ul>
-            <li>
-              Write something big, or random sories of yours.
-            </li>
-            <li>
-              Make your text interesting by using the tools above.
-            </li>
-          </ul>
-          <blockquote>
-            It's like being in a fairytale 👏
-            <br />
-            – Unfold Team
-          </blockquote>
-        `,
-        editorOption: {
-          // Some Quill options...
-          theme: 'snow',
-          modules: {
-            toolbar: [
-              ['bold', 'italic', 'underline', 'strike'],
-              ['blockquote', 'code-block']
-            ]
-          }
-        }
-      }
-    },
-    mounted() {
-    },
-    methods: {
-      onEditorBlur(editor) {
-        console.log('editor blur!', editor)
-      },
-      onEditorFocus(editor) {
-        console.log('editor focus!', editor)
-      },
-      onEditorReady(editor) {
-        console.log('editor ready!', editor)
-      }
-    }
+import editor from '../../components/editor';
+
+export default {
+  components: {
+    editor
   }
+}
 </script>
 
-<style lang="scss" scoped>
-  .container {
-    width: 60%;
-    margin: 0 auto;
-    padding: 100px 0;
-
-    .quill-editor {
-      min-height: 200px;
-      max-height: 400px;
-      overflow-y: auto;
-    }
-  }
+<style>
+.container {
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding-top: 100px;
+}
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+.links {
+  padding-top: 15px;
+}
 </style>
--- plugins/nuxt-quill-plugin.js
+++ plugins/nuxt-quill-plugin.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
 import VueQuillEditor from 'vue-quill-editor'
-
+import Quill from 'quill'
+var Font = Quill.import('formats/font');
+// We do not add Aref Ruqaa since it is the default
+Font.whitelist = ['mirza', 'roboto', 'inconsolata','monospace'];
+Quill.register(Font, true);
 Vue.use(VueQuillEditor)
